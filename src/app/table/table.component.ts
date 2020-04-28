53d259f236c2f3fa32650c18ba160b4fe8c813d6
ElenaGpe <64209608+ElenaGpe@users.noreply.github.com>
2020-04-28 01:44:40 -0500

    E3 A7

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { AUTOMOVILES } from './data';
+import { Automovil } from './models';
+import {NgbModule, ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent implements OnInit {
 
+export class TableComponent implements OnInit {
+  autos: Automovil[];
   constructor() { }
 
   ngOnInit(): void {
+    this.autos= AUTOMOVILES;
   }
 
 }
